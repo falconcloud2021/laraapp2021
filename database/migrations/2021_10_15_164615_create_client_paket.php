487999fddf05f94fbe6d4f69747547ed014cc358
falconcloud2021 <bondarchuck.yaroslaw@gmail.com>
2021-10-21 09:11:21 +0300

    init

---
+++ database/migrations/2021_10_15_164615_create_client_paket.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientPaket extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_paket', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('text')->nullable();
+            $table->string('meta')->nullable();
+            $table->string('desc')->nullable();
+            $table->tinyInteger('rating');
+            $table->tinyInteger('status');
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->string('token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_paket');
+    }
+}
